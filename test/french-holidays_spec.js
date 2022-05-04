7e7aaf64222d8c16a6ff37625316d2a07b852c10
tataille <jean.marc.taillant@gmail.com>
2022-05-04 17:20:37 +0200

    Adapt repo to fit node-red spec

---
+++ test/french-holidays_spec.js
@@ -0,0 +1,55 @@
+var helper = require("node-red-node-test-helper");
+var frenchHolidaysNode = require("../french-holidays/french-holidays.js");
+
+describe('french-holidays Node', function () {
+
+  afterEach(function () {
+    helper.unload();
+  });
+
+  it('should be loaded', function (done) {
+    var flow = [{ id: "n1", type: "french-holidays", name: "test name" }];
+    helper.load(frenchHolidaysNode, flow, function () {
+      var n1 = helper.getNode("n1");
+      n1.should.have.property('name', 'test name');
+      done();
+    });
+  });
+
+  it('should make payload contains holidays data', function (done) {
+    var flow = [
+      {
+          "id": "n1",
+          "type": "french-holidays",    
+          "name": "Vacances",
+          "academy": "Rennes",
+          "geo": "Métropole",
+          "wires": [
+              [
+                  "n2"
+              ]
+          ]
+      },
+      {
+          "id": "n2",
+          "type": "helper",
+          "name": "",
+          "wires": []
+      }
+  ];
+    helper.load(frenchHolidaysNode, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      n2.on("input", function (msg) {
+        try {
+          today = new Date();
+          msg.payload.should.have.property('day', today.getDay());
+          done();
+        } catch(err) {
+          done(err);
+        }
+      });
+      n1.receive({ payload: "test" });
+    });
+  }); 
+});
